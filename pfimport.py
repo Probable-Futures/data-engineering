3fb81d4c15350a91743be75e9b981271352f1665
Paul Ford <ford@ftrain.com>
2021-07-27 17:32:18 -0400

    PF importer.

--- pfimport.py
+++ pfimport.py
@@ -38,7 +38,6 @@ Futures database schema.
 WARMS = [0.5 * x for x in range(1,6)]
     
 @click.command()
-
 @click.option("--mutate", default=False, help="Set to True to write to database")
 @click.option("--conf", default="conf.yaml", help="YAML config file")
 @click.option(
@@ -54,76 +53,105 @@ WARMS = [0.5 * x for x in range(1,6)]
               default=None,
               help="Database username")
 @click.option("--dbpassword", nargs=1, default=None, help="Database password")
-@click.option("--coords", default=False, help="Insert coordinates")
-@click.option("--cdfs", default=False, help="Insert CDFs")
-@click.option("--coordsfile", default="models.yaml", help="Name of the models yaml file, typically models.yaml")
+@click.option("--load-coordinates", default=False, help="Insert coordinates")
+@click.option("--load-cdfs", default=False, help="Insert CDFs")
+@click.option("--log-sql", default=False, help="Log SQLAlchemy SQL calls")
 
 
-def __main__(mutate, conf, dbhost, dbname, dbuser, dbpassword, coords, cdfs, coordsfile):
+def __main__(mutate, conf, dbhost, dbname, dbuser, dbpassword, load_coordinates, load_cdfs, log_sql):
 
     # Load YAML file
     conf = safe_load(open(conf))
-    engine = create_engine('postgresql://' + dbuser + ':' + dbpassword + '@' + dbhost + '/' + dbname, echo=True)
+    engine = create_engine('postgresql://' + dbuser + ':' + dbpassword + '@' + dbhost + '/' + dbname, echo=log_sql)
     metadata = MetaData(schema='pf_public')
     metadata.reflect(engine)
     Base = automap_base(metadata=metadata)
     Base.prepare()
-    Dataset, Coordinates, Statistics, DatasetData = \
+    Dataset, Coordinates, Statistics, WarmingScenario, DatasetData = \
         Base.classes.pf_datasets, \
         Base.classes.pf_dataset_coordinates, \
         Base.classes.pf_dataset_statistics, \
+        Base.classes.pf_warming_scenarios, \
         Base.classes.pf_dataset_data
 
     Session = sessionmaker(bind=engine)
 
+    def to_hash(model, pts):
+        s = '{}SRID=4326;POINT({} {})'.format(model, pts[0], pts[1])
+        hashed = md5(s.encode()).hexdigest()
+        return hashed
+
     # We make a table of all possible coordinates
-    if coords is True and mutate is True:
+    if load_coordinates is True:
+        print("Loading coordinates using data in the config file.")
         
         with Progress() as progress:
             with Session() as session:
-                
                 task1 = progress.add_task("Loading coords", total=len(conf.get('models')))
 
                 for model in conf.get('models'):
                     name = model.get('model')
                     coords = list(itertools.product(model.get('lon'), model.get('lat')))
 
-                    session.query(Coordinates).filter(Coordinates.model==name).delete()
+                    if mutate:
+                        session.query(Coordinates).filter(Coordinates.model==name).delete()
 
-                    def save_record(coord, name):
+                    def to_record(coord, name):
                         pt = 'POINT({} {})'.format(*coord)
                         return (Coordinates(model=name, point=pt))
 
-                    records = [save_record(coord, name) for coord in coords]
-                    session.bulk_save_objects(records)                
-                    session.commit()
+                    records = [to_record(coord, name) for coord in coords]
+                    if mutate:
+                        session.bulk_save_objects(records)                
+                        session.commit()
                     progress.update(task1, advance=1)                    
 
-    if cdfs is True and mutate is True:    
+    if load_cdfs is True:
         with Progress() as progress:
 
             task1 = progress.add_task("Loading NetCDF files", total=len(conf.get('datasets')))
 
             for cdf in conf.get('datasets'):
 
-
                 progress.update(task1, advance=1)
                 daa = xarray.open_dataset(cdf.get('filename')).to_dict()
+
                 dims = [daa["coords"][x]["data"] for x in cdf.get('dimensions')]
 
+                task2 = progress.add_task("{}".format(cdf.get('name')),
+                                          total=len(cdf.get('variables')))
+
+
+                all = []
+                for r in range(len(daa.dims)):
+                    var = daa.dims[r] 
+                    a = daa.coords.indexes[dim]
+                    
                 for v in cdf.get('variables'):
                     var = v.get('name')
-                    a = itertools.product(*dims) # run it here to get the iterator
-                    data = daa["data_vars"][var]["data"]
-                    flat = deepflatten(data)
+                    data = daa['data_vars'][var]["data"]
+                    product = itertools.product(*dims) # run it here to get the iterator
+                    pprint([len(data), len(data[0]), len(data[0][0]), len(product)])
+                    exit(0)
+                    
                     zipped = zip(a,flat)
-                    ds = [[cdf.get('dataset'), var,
-                           'POINT({} {})'.format(*coord), [d]]
+                    dataset = cdf.get('dataset')
+                    model = cdf.get('model')                    
+                    
+                    ds = [[dataset,
+                           var,
+                           coord, [d]]
                            for coord, d in zipped]
-                    task2 = progress.add_task("Saving {}".format(cdf.get('name')), total=len(ds))
-                    pprint(ds[0:10])
+
                     with Session() as session:
-                        
+
+                        print("Deleting old data from {}".format(cdf['dataset']))
+                        # Delete old
+                        if mutate:
+                            session.query(DatasetData).filter(DatasetData.dataset_id==cdf['dataset']).delete()
+                            session.query(Dataset).filter(Dataset.id==cdf['dataset']).delete()
+
+                        # Add
                         d = Dataset(id = cdf['dataset'],
                                     name = cdf['name'],
                                     slug = cdf['slug'],
@@ -132,28 +160,42 @@ def __main__(mutate, conf, dbhost, dbname, dbuser, dbpassword, coords, cdfs, coo
                                     category = cdf['category'],
                                     model = cdf['model'],
                                     unit = cdf['unit'])
-                        session.add(d)
+                        print("Adding dataset {}".format(cdf['dataset']))
+                        if mutate:
+                            session.add(d)
 
-
-                        def save_record(dataset_id, var, coord, val):
-                            s = '{}{}'.format(cdf['model'], coord)
-                            # pprint(s)
-                            hashed = md5(s.encode()).hexdigest()
+                        def to_record(dataset_id, var, coord, val):
+                            hashed = to_hash(model, coord)
                             dd = DatasetData(dataset_id=dataset_id,
                                              warming_scenario=var,
                                              coordinate_hash=hashed,
                                              data_values=val)
+                            return dd
+                        
+                        # Add variables
+                        for v in cdf['variables']:
+                            print("Adding variable {}".format(v['name']))
+                            ws = WarmingScenario(slug=v['name'],
+                                                 name=v['long_name'],
+                                                 description=None)
 
-                            progress.update(task2, advance=1)                        
-                            session.add(dd)
+                            if mutate:
+                                session.query(WarmingScenario).filter(WarmingScenario.slug==v['name']).delete()
+                                session.add(ws)
 
-                        for rec in ds:
-                            save_record(*rec)
+                        records = [to_record(*rec) for rec in ds]
+                        if mutate:
 
-                        session.commit()
+                            # Doing this in bulk led to some weird async thing where the variables weren't in the database.
+                            for record in records:
+                                session.add(record)                                
 
-
-
+                        print("Saving data")
+                        if mutate:
+                            # session.bulk_save_objects(records)  
+                            session.commit()
+                        progress.update(task1, advance=1)
+                            
 
 if __name__ == "__main__":
     __main__()
