9b8f7fba9488b359b945c75a9166073e846e741f
Moustafa <moustafawehbe102@gmail.com>
2024-01-11 17:53:21 +0200

    fix: depds

--- mapbox-tileset-validation/tests/utils/configs.ts
+++ mapbox-tileset-validation/tests/utils/configs.ts
@@ -22,9 +22,9 @@ export const VALIDATION_METHOD: ValidationMethod = "using-checksums";
  * [4, 3, 5] is a tile inside the west tileset
  */
 
-export const TILESET_ID = "probablefutures.40614-west-v3";
+export const TILESET_ID = "probablefutures.40403-west-v3";
 
-export const CSV_FILE_PATH = `${woodwellDatasetDir}/woodwell.40614.csv`;
+export const CSV_FILE_PATH = `${woodwellDatasetDir}/woodwell.40403.csv`;
 
 // each element in the TILES array shoud be of the order: z, x, y
 export const TILES = [
@@ -34,64 +34,64 @@ export const TILES = [
   // [5, 15, 14],
   // [2, 2, 1],
   //west
-  // [4, 3, 5],
-  // [4, 3, 6],
-  // [3, 2, 4],
-  [3, 2, 3],
+  [4, 3, 5],
+  [4, 3, 6],
   [3, 2, 4],
+  // [3, 2, 3],
+  // [3, 2, 4],
 ];
 
-export const DATASET_UNIT: DatasetUnit = "days";
+export const DATASET_UNIT: DatasetUnit = "°C";
 
 // Check the CSV file and update the index of each column here. Index starts from 0.
 
 // using mean
-// export const COLUMNS_INDEXES_IN_CSV = {
-//   lon: 0, // if lon and lat are in the same column set them both to the same index of that column.
-//   lat: 1,
-//   data_baseline_low: 3,
-//   data_baseline_mid: 2,
-//   data_baseline_high: 5,
-//   data_1c_low: 7,
-//   data_1c_mid: 6,
-//   data_1c_high: 9,
-//   data_1_5c_low: 11,
-//   data_1_5c_mid: 10,
-//   data_1_5c_high: 13,
-//   data_2c_low: 15,
-//   data_2c_mid: 14,
-//   data_2c_high: 17,
-//   data_2_5c_low: 19,
-//   data_2_5c_mid: 18,
-//   data_2_5c_high: 21,
-//   data_3c_low: 23,
-//   data_3c_mid: 22,
-//   data_3c_high: 25,
-// };
-
-// data using median
 export const COLUMNS_INDEXES_IN_CSV = {
   lon: 0, // if lon and lat are in the same column set them both to the same index of that column.
   lat: 1,
   data_baseline_low: 3,
-  data_baseline_mid: 4,
+  data_baseline_mid: 2,
   data_baseline_high: 5,
   data_1c_low: 7,
-  data_1c_mid: 8,
+  data_1c_mid: 6,
   data_1c_high: 9,
   data_1_5c_low: 11,
-  data_1_5c_mid: 12,
+  data_1_5c_mid: 10,
   data_1_5c_high: 13,
   data_2c_low: 15,
-  data_2c_mid: 16,
+  data_2c_mid: 14,
   data_2c_high: 17,
   data_2_5c_low: 19,
-  data_2_5c_mid: 20,
+  data_2_5c_mid: 18,
   data_2_5c_high: 21,
   data_3c_low: 23,
-  data_3c_mid: 24,
+  data_3c_mid: 22,
   data_3c_high: 25,
 };
+
+// data using median
+// export const COLUMNS_INDEXES_IN_CSV = {
+//   lon: 0, // if lon and lat are in the same column set them both to the same index of that column.
+//   lat: 1,
+//   data_baseline_low: 3,
+//   data_baseline_mid: 4,
+//   data_baseline_high: 5,
+//   data_1c_low: 7,
+//   data_1c_mid: 8,
+//   data_1c_high: 9,
+//   data_1_5c_low: 11,
+//   data_1_5c_mid: 12,
+//   data_1_5c_high: 13,
+//   data_2c_low: 15,
+//   data_2c_mid: 16,
+//   data_2c_high: 17,
+//   data_2_5c_low: 19,
+//   data_2_5c_mid: 20,
+//   data_2_5c_high: 21,
+//   data_3c_low: 23,
+//   data_3c_mid: 24,
+//   data_3c_high: 25,
+// };
 
 // only mid
 // export const COLUMNS_INDEXES_IN_CSV = {
